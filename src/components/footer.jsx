9b41edc2ee2391b037b2f7f062dd278ae8e49320
Roh1121 <dollyavula09@gmail.com>
2025-05-05 17:39:43 +0530

    Initial commit

---
+++ src/components/footer.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+// Footer Component
+const Footer = () => {
+    return (
+        <footer>
+            <p>
+                Created  by
+                <a href="https://www.linkedin.com/in/rohini-avula-234597267/" target="_blank" rel="noopener noreferrer" className="link">
+                    <span> </span>
+                    Rohini Avula
+                </a>
+            </p>
+        </footer>
+    );
+}
+ 
+export default Footer;
